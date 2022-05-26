50f9b146e648faee3650ba458f1f83593212e2e0
PabloGolobar <86614673+PabloGolobar@users.noreply.github.com>
2022-05-26 21:05:50 +0300

    Made main func fo server.go

--- server_app/server.go
+++ server_app/server.go
@@ -1,15 +1,24 @@
-package server_app
+package main
 
 import (
 	"context"
 	"fmt"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 	pokemonpc "grpc-client-server/proto_app"
 	"log"
+	"net"
+	"os"
+	"os/signal"
 )
+
+const defaultPort = "4041"
 
 var collection *mongo.Collection
 
@@ -61,4 +70,54 @@ func (s *server) CreatePokemon(ctx context.Context, request *pokemonpc.CreatePok
 			Description: pokemon.GetDescription(),
 		},
 	}, nil
+}
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+	mongo_uri := os.Getenv("MONGODB_URL")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Println("Connecting to MongoDB...")
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongo_uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Pokemon Service started")
+	collection = client.Database("pokemonDB").Collection("pokemon")
+	listen, err := net.Listen("tcp", "0.0.0.0:4041")
+	if err != nil {
+		log.Fatal("Failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	newServer := grpc.NewServer(opts...)
+	pokemonpc.RegisterPokemonServiceServer(newServer, &server{})
+	reflection.Register(newServer)
+
+	go func() {
+		fmt.Println("Starting Server...")
+		if err := newServer.Serve(listen); err != nil {
+			log.Fatal("Failed to serve : %v", err)
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+	fmt.Println("Closing MongoDB Collection")
+	if err := client.Disconnect(context.TODO()); err != nil {
+		log.Fatal("Error on disconection with MongoDB : %V", err)
+	}
+	fmt.Println("Stopping the server")
+	newServer.Stop()
+	log.Println("End of programm")
 }
